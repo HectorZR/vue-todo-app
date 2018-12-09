d9195a2bb22351e7d1a19de0d5f6ea96c4f87dfc
Hector Zurga <hjzurga@gmail.com>
2018-12-09 19:46:22 -0400

    updating list

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,11 +1,11 @@
 <template>
-  <div >
-    <!-- <form id="form-component">
+  <div class="container">
+    <form id="form-component">
       <input type="text" name="task" id="">
       <button v-on:click="pushTask">Save Task</button>
-    </form> -->
-    <div>
-      <ul>
+    </form>
+    <div class="list-container">
+      <ul class="list" >
         <li v-for="(todo, index) in todos" v-bind:key="index">
           {{ todo.text }}
         </li>
@@ -15,17 +15,33 @@
 </template>
 
 <script >
-  export default {
-    data() {
-      return{
-        todos: [
-          { text: 'First to render' },
-          { text: 'Second to render' },
-          { text: 'Third to render' },
-        ],
-      }
-    },
-  };
+export default {
+  data() {
+    return {
+      todos: [
+        { text: 'first to render' },
+      ],
+    };
+  },
+  methods: {},
+};
 </script>
 
+<style lang="scss" scoped>
+// .container{
+//   display: flex;
+//   align-content: center;
+//   flex-direction: center;
+// }
+.list-container {
+  width: 50%;
+  background-color: green;
+  color: white;
 
+  .list {
+    background-color: blue;
+    width: 50%;
+    list-style-type: none;
+  }
+}
+</style>
