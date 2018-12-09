517caef5c6c4e03d04abfa082d01815b5098582e
Hector Zurga <hjzurga@gmail.com>
2018-12-09 19:14:35 -0400

    getting a rendered list done

---
+++ src/components/Input.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+
+export default {
+  name: 'input',
+};
+</script>
+
+<style lang="scss" scoped>
+  .form-component {
+    background-color: aquamarine;
+  }
+</style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,17 +1,32 @@
 <template>
-  <div>
-    Hello World
+  <div >
+    <!-- <form id="form-component">
+      <input type="text" name="task" id="">
+      <button v-on:click="pushTask">Save Task</button>
+    </form> -->
+    <div>
+      <ul>
+        <li v-for="(todo, index) in todos" v-bind:key="index">
+          {{ todo.text }}
+          {{ index }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
-<script>
-
-export default {
-  name: 'todo',
-  components: {
-    
-  }
-}
+<script >
+  export default {
+    data() {
+      return{
+        todos: [
+          { text: 'First to render' },
+          { text: 'Second to render' },
+          { text: 'Third to render' },
+        ],
+      }
+    },
+  };
 </script>
 
 
